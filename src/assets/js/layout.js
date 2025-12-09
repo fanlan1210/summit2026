2d36746ef62397ad2c34eead9bd4585be2acfc11
Link1515 <h26281660@gmail.com>
2025-12-09 17:36:49 +0800

    fix: adjust navbar styling

--- src/assets/js/layout.js
+++ src/assets/js/layout.js
@@ -5,16 +5,34 @@ $(function () {
    * ============================================
    */
   // 下滾動態改變導航顏色
-  const navbarEl = $('#navbar');
-  function onScroll() {
-    $(window).scrollTop() > 10 ? navbarEl.addClass('bg-white') : navbarEl.removeClass('bg-white');
+  const navTransparentFlag = Boolean($('#nav-transparent-flag')[0]);
+  const $navbar = $('#navbar');
+  const $mobileNav = $('#mobile-nav');
+
+  if (navTransparentFlag) {
+    $navbar.addClass('bg-transparent transition-colors');
+    function onScroll() {
+      if ($(window).scrollTop() > 10) {
+        $navbar.addClass('bg-white shadow-md');
+      } else {
+        $navbar.removeClass('bg-white shadow-md');
+      }
+    }
+    $(window).on('scroll', onScroll);
+    onScroll();
+  } else {
+    $navbar.addClass('bg-white shadow-md');
   }
-  $(window).on('scroll', onScroll);
-  onScroll();
 
   // toggle 選單
   $('#toggle-nav-btn').on('click', function (e) {
     e.preventDefault();
-    $('#mobile-nav').toggleClass('grid-rows-[0fr] grid-rows-[1fr]');
+    $navbar.addClass('bg-white');
+    $mobileNav.toggleClass('grid-rows-[0fr] grid-rows-[1fr]');
+    $mobileNav.toggleClass('shadow-md');
+
+    if ($(window).scrollTop() < 10 && $mobileNav.hasClass('grid-rows-[0fr]')) {
+      $navbar.removeClass('bg-white');
+    }
   });
 });
