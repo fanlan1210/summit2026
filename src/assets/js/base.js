7c45bd4637a642ff72f37eeeddd25ce09dc0aac9
Link1515 <h26281660@gmail.com>
2025-12-10 11:01:20 +0800

    feat: add language selector

--- src/assets/js/base.js
+++ src/assets/js/base.js
@@ -15,11 +15,20 @@ $('#cookie-notice button').on('click', function (e) {
   $('#cookie-notice').addClass('hidden')
 })
 
-$('#lang-select').on('change', function (e) {
-  let lang = $(this).val()
+function changeLang(lang) {
   console.log("lang changed", lang)
   setLocale(lang)
   storage.setItem("userLang", lang)
+}
+
+$('.zh-btn').on('click', function (e) {
+  changeLang('zh')
+  toggleMobileNav()
+})
+
+$('.en-btn').on('click', function (e) {
+  changeLang('en')
+  toggleMobileNav()
 })
 
 $(function () {
@@ -87,3 +96,36 @@ $(document).on('keydown', function (e) {
     $('#modal .modal-close').click()
   }
 })
+
+// Change the navbar color dynamically on scroll
+const navTransparentFlag = Boolean($('#nav-transparent-flag')[0]);
+const $navbar = $('#navbar');
+const $mobileNav = $('#mobile-nav');
+
+if (navTransparentFlag) {
+  $navbar.addClass('bg-transparent transition-colors');
+  function onScroll() {
+    if ($(window).scrollTop() > 10) {
+      $navbar.addClass('bg-white shadow-md');
+    } else {
+      $navbar.removeClass('bg-white shadow-md');
+    }
+  }
+  $(window).on('scroll', onScroll);
+  onScroll();
+} else {
+  $navbar.addClass('bg-white shadow-md');
+}
+
+// toggle mobile nav
+$('#toggle-nav-btn').on('click', toggleMobileNav);
+
+function toggleMobileNav() {
+  $navbar.addClass('bg-white');
+  $mobileNav.toggleClass('grid-rows-[0fr] grid-rows-[1fr]');
+  $mobileNav.toggleClass('shadow-md');
+
+  if ($(window).scrollTop() < 10 && $mobileNav.hasClass('grid-rows-[0fr]')) {
+    $navbar.removeClass('bg-white');
+  }
+}
