fd7bcb81b65997db301e6cf8bc6751053b6e92c2
Link1515 <terrylin1515@gmail.com>
2026-02-14 02:17:36 +0800

    chore: set src alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+// console.error(import.meta.env.PROD)
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
-});
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '~': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  },
+})
