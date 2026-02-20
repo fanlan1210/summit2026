570825a9023122da79d574caf16c707b6c0af427
Link1515 <terrylin1515@gmail.com>
2026-02-20 21:44:54 +0800

    refactor: make stripProjectName private

--- src/i18n/utils.js
+++ src/i18n/utils.js
@@ -6,13 +6,6 @@ const PROJECT_NAME = import.meta.env.BASE_URL?.replaceAll(/^\/+|\/+$/g, '')
 const defaultLocale = i18n.defaultLocale
 const locales = i18n.locales
 
-function normalizePath(path) {
-  if (!path.startsWith('/')) path = `/${path}`
-  path = path.replace(/\/+/g, '/')
-  if (path.length > 1 && path.endsWith('/')) path = path.slice(0, -1)
-  return path
-}
-
 export function getLocaleFromPath(pathname) {
   pathname = stripProjectName(pathname)
   const p = normalizePath(pathname)
@@ -21,7 +14,14 @@ export function getLocaleFromPath(pathname) {
   return defaultLocale
 }
 
-export function stripProjectName(pathname) {
+function normalizePath(path) {
+  if (!path.startsWith('/')) path = `/${path}`
+  path = path.replace(/\/+/g, '/')
+  if (path.length > 1 && path.endsWith('/')) path = path.slice(0, -1)
+  return path
+}
+
+function stripProjectName(pathname) {
   const p = normalizePath(pathname)
   return PROJECT_NAME && p.startsWith(`/${PROJECT_NAME}`) ? p.slice(PROJECT_NAME.length + 1) : p
 }
