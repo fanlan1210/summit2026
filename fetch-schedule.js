fdcc9122c65251fe92823b5463c8fd6c5acf6461
Link1515 <terrylin1515@gmail.com>
2026-02-26 17:59:29 +0800

    chore: add script to fetch schedule data

---
+++ fetch-schedule.js
@@ -0,0 +1,13 @@
+import fs from 'fs/promises'
+import path from 'path'
+import 'dotenv/config'
+import { scheduleToJson } from 'opass-schedule-to-json'
+
+const schedule = await scheduleToJson({
+  apiKey: process.env.GCP_API_KEY,
+  spreadsheetId: process.env.SPREADSHEET_KEY,
+  avatarBaseUrl: process.env.AVATAR_BASE_URL,
+  defaultAvatar: process.env.DEFAULT_AVATAR,
+})
+
+fs.writeFile(path.resolve('src/data/schedule.json'), JSON.stringify(schedule, null, 2), 'utf-8')
